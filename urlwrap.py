986f99b6ff8e5715c499f927408a31222ff7c257
Paul Salden <paul.salden@gmail.com>
2014-07-10 14:03:10 +0200

    fixed highlight detection

--- urlwrap.py
+++ urlwrap.py
@@ -30,7 +30,7 @@ def _c(option, bgoption=False):
             weechat.config_color(weechat.config_get(bgoption)))))
     return weechat.color(weechat.config_color(weechat.config_get(option)))
 
-def _reconstruct_print(string, highlighted):
+def _reconstruct_print(prefix, message, highlighted):
     # as printing without alignment also strips timestamp and delimiter,
     # they must be reconstructed
     timestamp = strftime(_s("weechat.look.buffer_time_format"))
@@ -38,26 +38,34 @@ def _reconstruct_print(string, highlighted):
         l if l.isdigit() else _c("weechat.color.chat_time_delimiters")+l+_c("weechat.color.chat_time")
         for l in timestamp])
 
-    nick, message = string.split("\t", 1)
     if highlighted:
-        nick = "".join((_c("weechat.color.chat_highlight", "weechat.color.chat_highlight_bg"),
-            weechat.string_remove_color(nick, "")))
-    nick = "".join((_c("weechat.color.chat_nick_prefix"), _s("weechat.look.nick_prefix"),
-        nick, _c("weechat.color.chat_nick_suffix"), _s("weechat.look.nick_suffix"),
+        prefix = "".join((_c("weechat.color.chat_highlight", "weechat.color.chat_highlight_bg"),
+            weechat.string_remove_color(prefix, "")))
+    prefix = "".join((_c("weechat.color.chat_nick_prefix"), _s("weechat.look.nick_prefix"),
+        prefix, _c("weechat.color.chat_nick_suffix"), _s("weechat.look.nick_suffix"),
         weechat.color("reset")))
 
     delimiter = "".join((" ", _c("weechat.color.chat_delimiters"), _s("weechat.look.prefix_suffix"),
         weechat.color("reset"), " ")) if _s("weechat.look.prefix_align") != "none" else " "
 
-    return "{} {}{}{}".format(timestamp, nick, delimiter, message)
+    return "{} {}{}{}".format(timestamp, prefix, delimiter, message)
 
 def modifier_cb(data, modifier, modifier_data, string):
     if "irc_privmsg" in modifier_data and ("http://" in string or "https://" in string):
         buffer = weechat.buffer_search("irc", modifier_data.split(";")[1])
-        nick = weechat.buffer_get_string(buffer, "localvar_nick")
-        highlighted = nick in string
+        mynick = weechat.buffer_get_string(buffer, "localvar_nick")
+
+        taglist = modifier_data.split(";")[2].split(",")
+        for tag in taglist:
+            if tag[:5] == "nick_":
+                nick = tag[5:]
+                break
+
+        prefix, message = string.split("\t", 1)
+
+        highlighted = nick != mynick and weechat.string_has_highlight(message, mynick)
         weechat.prnt_date_tags(buffer, 0, "urlwrap_filter_tag", string)
-        weechat.prnt(buffer, "\t\t{}".format(_reconstruct_print(string, highlighted)))
+        weechat.prnt(buffer, "\t\t{}".format(_reconstruct_print(prefix, message, highlighted)))
         return ""
     return string
 
